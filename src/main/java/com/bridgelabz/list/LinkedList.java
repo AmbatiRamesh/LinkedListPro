8fffb56605d0cde11d9128fd493e63306abad0e2
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-07-10 11:00:32 +0530

    Added: add elements in linked list

--- src/main/java/com/bridgelabz/list/LinkedList.java
+++ src/main/java/com/bridgelabz/list/LinkedList.java
@@ -15,15 +15,33 @@ public class LinkedList {
         Node newNode = new Node(data);
         if (head == null) {
             head = newNode;
+            tail = newNode;
         } else {
             tail.next = newNode;
+            tail = newNode;
         }
-        tail = newNode;
+    }
+    public void display() {
+        Node current = head;
+        if(head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        while(current != null) {
+            if(current.next != null)
+                System.out.print(current.data + "->");
+            else
+                System.out.println(current.data + "\n");
+            current = current.next;
+        }
     }
     public static void main(String[] args) {
         LinkedList link = new LinkedList();
         link.addNode(56);
+        link.display();
         link.addNode(30);
+        link.display();
         link.addNode(76);
+        link.display();
     }
 }
